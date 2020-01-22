12ebb0e25e190339aaf1a62c65fa04721f10e02d
The Big Cheese <yohan.lefol@gmail.com>
2020-01-22 15:26:21 +0100

    Crypt password trial

--- R_Work_folder/Archived/server_connection.R
+++ R_Work_folder/Archived/server_connection.R
@@ -4,7 +4,15 @@ if(!require(RMySQL)){
   library(RMySQL)
 }
 
+if(!require(keyring)){
+  install.packages("keyring")
+  library(keyring)
+}
 
-DB <- dbConnect(RMySQL::MySQL(), user="cellomet1", host="sql25.webmo.fr",password="cellomet1", dbname="cellomet1")
+#key_set("ddb")
 
-dbGetQuery(DB, paste0("INSERT INTO `questionnaire` (`Email`, `Type_of_Study`, `Comments`, `submit_date`) VALUES ('yohan.lefol@gmail.com', 'test_connect_real', '#3', '2020-01-21');"))
+
+
+DB <- dbConnect(RMySQL::MySQL(), user="cellomet1", host="sql25.webmo.fr",password=key_get("ddb"), dbname="cellomet1")
+
+dbGetQuery(DB, paste0("INSERT INTO `questionnaire` (`Email`, `Type_of_Study`, `Comments`, `submit_date`) VALUES ('yohan.lefol@gmail.com', 'test_connect_real_marie', '#3', '2020-01-21');"))
