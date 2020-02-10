766e4496d4312558eb20412e8963fae24cc19be0
The Big Cheese <yohan.lefol@gmail.com>
2020-02-10 17:10:16 +0100

    More tests on server connection

--- R_Work_folder/Archived/server_connection.R
+++ R_Work_folder/Archived/server_connection.R
@@ -4,11 +4,14 @@ if(!require(RMySQL)){
   library(RMySQL)
 }
 
+#Establish the connection
+DB <- dbConnect(RMySQL::MySQL(), user="test", host="sqlgold.webmo.fr:50413",password="test", dbname="cellomet2")
 
-DB <- dbConnect(RMySQL::MySQL(), user="test", host="sql10413.webmo.fr",password="test", dbname="cellomet1")
+#Establish the connection
+DB <- dbConnect(RMySQL::MySQL(), user="test", host="sql10413.webmo.fr",password="test", dbname="cellomet2")
 
-dbGetQuery(DB, "show GRANTS;")
+#Send the query and return results
+dbGetQuery(DB, "SELECT * from `questionnaire`;")
 
 
 
-dbGetQuery(DB, paste0("INSERT INTO `questionnaire` (`Email`, `Type_of_Study`, `Comments`, `submit_date`) VALUES ('yohan.lefol@gmail.com', 'test_connect_real_config_marie', '#3', '2020-01-21');"))
